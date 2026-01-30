3d63960ed03fc5f5eb08b64b4d5c206ac1b99774
Lyj <3218406937@qq.com>
2026-01-31 00:28:53 +0800

    开发我的中间及路由跳转

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { viteMockServe } from 'vite-plugin-mock'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,6 +16,12 @@ export default defineConfig({
       mockPath: 'mock',
       enable: true,
       logger: true
+    }),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/svg')],
+      symbolId: 'icon-[dir]-[name]',
+      inject: 'body-last',
+      customDomId: '__svg__icons__dom__'
     })
   ],
   resolve: {
